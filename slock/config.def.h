abb4772aab5eae49d99b85068951e8afa72e1544
tuilk <likh.aini@outlook.com>
2022-08-12 19:14:16 +0800

    apply patch -> fore-and-back

--- slock/config.def.h
+++ slock/config.def.h
@@ -10,3 +10,31 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 0;
+
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 75;
+/* grid width and height for right center alignment */
+static const int logow = 12;
+static const int logoh = 6;
+
+static XRectangle rectangles[9] = {
+	/* x    y       w       h */
+	{ 0,    3,      1,      3 },
+	{ 1,    3,      2,      1 },
+	{ 0,    5,      8,      1 },
+	{ 3,    0,      1,      5 },
+	{ 5,    3,      1,      2 },
+	{ 7,    3,      1,      2 },
+	{ 8,    3,      4,      1 },
+	{ 9,    4,      1,      2 },
+	{ 11,   4,      1,      2 },
+};
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius=5;
+/*Enable Pixelation*/
+//#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize=0;
